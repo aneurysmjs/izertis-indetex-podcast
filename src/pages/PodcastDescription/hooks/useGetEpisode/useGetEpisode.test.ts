3fd63b88c1afcfc4b30b6822c2c53e9081e7ede7
aneurysmjs <aneurysmjs@outlook.com>
2024-10-22 23:14:15 +0200

    test(useGetEpisode): add test

---
+++ src/pages/PodcastDescription/hooks/useGetEpisode/useGetEpisode.test.ts
@@ -0,0 +1,37 @@
+import { expect, it, vi, afterEach } from 'vitest';
+import { renderHook, waitFor } from '@testing-library/react';
+
+import { WithQueryWrapper } from '@/utils/testUtils/renderWithQueryClient';
+import podcastDescriptionMockData from '@/services/podcastDescriptionMockData';
+
+import useGetEpisode from './useGetEpisode';
+
+const { mockGetPodcastDescription } = vi.hoisted(() => ({
+  mockGetPodcastDescription: vi.fn(),
+}));
+
+vi.mock('@/services/podcastService', () => ({
+  getPodcastDescription: mockGetPodcastDescription,
+}));
+
+describe('useGetEpisode', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return an episode', async () => {
+    mockGetPodcastDescription.mockResolvedValue({ data: podcastDescriptionMockData });
+
+    const { result } = renderHook(() => useGetEpisode('1', '1000608903551'), {
+      wrapper: WithQueryWrapper,
+    });
+
+    expect(mockGetPodcastDescription).toHaveBeenCalledWith('1');
+    expect(result.current.isLoading).toBe(true);
+
+    await waitFor(() => {
+      expect(result.current.data).toStrictEqual(podcastDescriptionMockData.results[1]);
+      expect(result.current.isLoading).toBe(false);
+    });
+  });
+});
--- src/pages/PodcastDescription/hooks/useGetEpisode/useGetEpisode.ts
+++ src/pages/PodcastDescription/hooks/useGetEpisode/useGetEpisode.ts
@@ -3,8 +3,6 @@ import { useQuery } from '@tanstack/react-query';
 import { getPodcastDescription } from '@/services/podcastService';
 import episodeAdapter from '@/pages/PodcastDescription/adapters/episodeAdapter';
 
-// import useGetPodcastDescription from '@/pages/PodcastDescription/hooks/useGetPodcastDescription';
-
 export default function useGetEpisode(id: string, episodeId: string) {
   return useQuery({
     queryFn: () => getPodcastDescription(id),
